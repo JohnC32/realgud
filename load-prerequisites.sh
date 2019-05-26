daafa9ce63eceafd81cfa75a0f8e0c6e9c69d480
rocky <rocky@gnu.org>
2019-05-26 05:22:53 -0400

    Travis take 7

--- load-prerequisites.sh
+++ load-prerequisites.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 emacs -batch -q --no-site-file -eval \
-  '(progn (package-initialize t) (dolist (package (quote (loc-changes load-relative test-simple))) (package-install package)))'
+  '(progn (package-initialize) (dolist (package (quote (loc-changes load-relative test-simple))) (package-install package)))'
